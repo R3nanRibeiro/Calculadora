e2ae9ce0a4a2f592b08abe1b1bf7b3641ae9cb5b
Renan Ribeiro <renanribeirodasilva92@gmail.com>
2026-05-04 00:12:08 -0300

    Update Operacao.cs

--- Calculadora/Operacao.cs
+++ Calculadora/Operacao.cs
@@ -1,6 +1,5 @@
 ﻿namespace Calculadora
 {
-    // Classe abstrata: define o "contrato" que toda operação deve seguir
     public abstract class Operacao
     {
         public float Numero1 { get; set; }
@@ -12,10 +11,8 @@
             Numero2 = numero2;
         }
 
-        // Método abstrato: cada subclasse é obrigada a implementar
         public abstract float Calcular();
 
-        // Método virtual: pode ser sobrescrito se necessário
         public virtual string Descricao()
         {
             return $"Operação entre {Numero1} e {Numero2}";
